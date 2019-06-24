f7f15e1a4bc6e103c12db0d97ee055567b712666
Hamed Rostami <h4medrostami@gmail.com>
2019-06-24 15:21:48 +0430

    add readme

---
+++ Time.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func localize(timeStr time.Time, location string) time.Time {
+	tl, _ := time.LoadLocation(location)
+	now := timeStr.In(tl)
+	return now
+}
+
+func main() {
+	q := time.Now().UTC() // sample UTC 0
+	temp := localize(q, "Asia/Istanbul")
+	fmt.Println(q)
+	fmt.Println(temp)
+}
